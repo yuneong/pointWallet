29cb1e86cefefd374b9c102ad095d5000cfd6893
oyy <oyy0821@chunjae.co.kr>
2025-07-03 13:53:23 +0900

    initial commit

---
+++ src/main/java/com/study/pointwallet/PointWalletApplication.java
@@ -0,0 +1,13 @@
+package com.study.pointwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointWalletApplication.class, args);
+    }
+
+}
